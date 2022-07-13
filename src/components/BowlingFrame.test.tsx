3f17ebbd06e7655e8abb0e1a332f29a9e1bcef6b
Jonathan Battisti <jbattisti@spiria.com>
2022-07-13 20:32:32 +0000

    Add rollScore in BowlingFrame

--- src/components/BowlingFrame.test.tsx
+++ src/components/BowlingFrame.test.tsx
@@ -5,32 +5,49 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import BowlingFrame from './BowlingFrame';
-import { range } from '../Utils';
+import BowlingFrame, { BowlingFrameProps, PinBoxValue } from './BowlingFrame';
 
-const renderBowlingFrame = (expectedNumberOfPinBox: number) => {
-    render(<BowlingFrame numberOfPinBox={expectedNumberOfPinBox} />);
+
+const renderBowlingFrame = (expectedBowlingFrameProps: BowlingFrameProps) => {
+    render(<BowlingFrame pinBoxValues={expectedBowlingFrameProps.pinBoxValues} frameScore={expectedBowlingFrameProps.frameScore} />);
 };
 
-const expectToContainAllPinbox = (expectedNumberOfPinBox: number) => {
+const expectToContainAllPinboxWithRollScore = (expectedPinBoxValues: Array<PinBoxValue>) => {
     const bowlingFrame = screen.getByTestId('bowlingFrame');
-    range(1, expectedNumberOfPinBox).map(number => {
-        expect(bowlingFrame).toContainElement(screen.getByTestId(`pinBox-${number.toString()}`));
+    expectedPinBoxValues.map(pinBoxValue => {
+        const pinBox = screen.getByTestId(`pinBox-${pinBoxValue.pinBoxNumber.toString()}`);
+        expect(bowlingFrame).toContainElement(pinBox);
+        expect(pinBox).toHaveTextContent(pinBoxValue.pinBoxRollScore);
     });
 };
 
 describe('<BowlingFrame />', () => {
     describe('props', () => {
-        it('should show 2 pinBoxes if numberOfPinBox is 2', () => {
-            const expectedNumberOfPinBox = 2;
-            renderBowlingFrame(expectedNumberOfPinBox);
-            expectToContainAllPinbox(expectedNumberOfPinBox);
+        it('should show 2 pinBoxes if numberOfPinBox is 2 with rollScore', () => {
+            const expectedBowlingFrameProps: BowlingFrameProps = {
+                pinBoxValues: [{ pinBoxNumber: 1, pinBoxRollScore: '2' }, { pinBoxNumber: 2, pinBoxRollScore: '7' }],
+                frameScore: '5'
+            };
+            renderBowlingFrame(expectedBowlingFrameProps);
+            expectToContainAllPinboxWithRollScore(expectedBowlingFrameProps.pinBoxValues);
         });
 
-        it('should show 3 pinBoxes if numberOfPinBox is 3', () => {
-            const expectedNumberOfPinBox = 3;
-            renderBowlingFrame(expectedNumberOfPinBox);
-            expectToContainAllPinbox(expectedNumberOfPinBox);
+        it('should show 3 pinBoxes if numberOfPinBox is 3 with rollScore', () => {
+            const expectedBowlingFrameProps: BowlingFrameProps = {
+                pinBoxValues: [{ pinBoxNumber: 1, pinBoxRollScore: '2' }, { pinBoxNumber: 2, pinBoxRollScore: '7' }, { pinBoxNumber: 3, pinBoxRollScore: '1' }],
+                frameScore: '7'
+            };
+            renderBowlingFrame(expectedBowlingFrameProps);
+            expectToContainAllPinboxWithRollScore(expectedBowlingFrameProps.pinBoxValues);
+        });
+
+        it('should show 2 pinBoxes if rollScore is strike', () => {
+            const expectedBowlingFrameProps: BowlingFrameProps = {
+                pinBoxValues: [{ pinBoxNumber: 1, pinBoxRollScore: 'X' }, { pinBoxNumber: 2, pinBoxRollScore: '' }],
+                frameScore: '10'
+            };
+            renderBowlingFrame(expectedBowlingFrameProps);
+            expectToContainAllPinboxWithRollScore(expectedBowlingFrameProps.pinBoxValues);
         });
 
     });
--- src/components/BowlingFrame.tsx
+++ src/components/BowlingFrame.tsx
@@ -1,17 +1,24 @@
 import React, { FC, ReactElement } from 'react';
-import { range } from '../Utils';
 import PinBox from './PinBox';
 
-type BowlingFrameProps = {
-    numberOfPinBox: number;
+export type PinBoxValue = {
+    pinBoxNumber: number;
+    pinBoxRollScore: string;
+};
+
+export type BowlingFrameProps = {
+    pinBoxValues: Array<PinBoxValue>;
+    frameScore: string;
 };
 
 const BowlingFrame: FC<BowlingFrameProps> = (props): ReactElement => {
+    const pinBoxes = props.pinBoxValues.map(pinBoxValue =>
+        <PinBox key={pinBoxValue.pinBoxNumber} number={pinBoxValue.pinBoxNumber} rollScore={pinBoxValue.pinBoxRollScore} />
+    );
+
     return (
         <div data-testid="bowlingFrame">
-            {range(1, props.numberOfPinBox).map(number =>
-                <PinBox key={number} number={number} rollScore={'2'} />
-            )}
+            {pinBoxes}
         </div>
     );
 };
